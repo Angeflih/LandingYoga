3a36e646a79959364cd09c22e6e451a5966300c0
Nastia <angeflih@gmail.com>
2020-04-05 20:22:36 +0300

    server

---
+++ js/user.js
@@ -0,0 +1,8 @@
+
+// let age = document.getElementById('age');
+ 
+// function showUser(surname, name) {
+//          alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+// }
+ 
+// showUser.apply(age, ["Горький","Максим"]);
